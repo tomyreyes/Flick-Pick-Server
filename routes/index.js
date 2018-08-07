55911482317b7a89bf3cad79c401fdf3063fdd2f
Tomy <tomyleereyes@gmail.com>
2018-08-07 00:02:32 -0700

    Redis setup for get-movie endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,42 @@
 const express = require('express')
 const router = express.Router()
 const axios = require('axios')
+const redis = require('redis')
+const client = redis.createClient()
 
 router.post('get-movie', async (req, res) => {
-	try {
-		const { id } = req.body
-		const movie = await axios({
-			method: 'GET',
-			url: `https://api.themoviedb.org/3/movie/${id}`,
-			params: {
-				api_key: '2d1610b0077610c43b2fe59ad827cfec'
-			}
-		})
-		res.send(movie)
-	} catch (err) {
-		console.log(err)
-	}
+	const { id } = req.body
+	client.hgetall(id, (error, foundMovie) => {
+		if (error) {
+			return console.log(error)
+		}
+		if (foundMovie) {
+			res.send(foundMovie)
+		} else {
+			const movie = await axios({
+				method: 'GET',
+				url: `https://api.themoviedb.org/3/movie/${id}`,
+				params: {
+					api_key: '2d1610b0077610c43b2fe59ad827cfec'
+				}
+			})
+			const { backdrop_path, original_title, overview, poster_path, release_date, vote_average } = movie
+			client.hset(id, [
+				'backdrop_path', backdrop_path,
+				'original_title', original_title,
+				'overview', overview,
+				'poster_path', poster_path,
+				'release_date', release_date,
+				'vote_average', vote_average
+			], (error, reply) => {
+				if (error) {
+					console.log(error)
+				}
+				console.log(reply)
+				res.send(movie)
+			})
+		}
+	})
 })
 
 router.get('get-featured', async (req, res) => {
